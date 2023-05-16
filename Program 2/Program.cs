96cacb70f8e72ddbb41da9bec520eb38a1cbe2e0
Luneoo <pooikpl@gmail.com>
2023-05-16 23:32:31 +0200

    Add project files.

---
+++ Program 2/Program.cs
@@ -0,0 +1,17 @@
+﻿string Name = "Ewa";
+bool Gender = true;
+int Age = 33;
+
+if (Gender == true && Age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else  if (Gender == true && Age >= 30)
+{
+    Console.WriteLine("Kobieta mająca 30 lat lub więcej");
+}
+else if (Gender == false && Age < 18)
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
+else Console.WriteLine("Pełnoletni mężczyzna");
